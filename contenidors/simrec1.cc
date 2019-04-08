6088a76650a88fcb1236a3f59077dfb645af33d1
PEP <borrellpep@gmail.com>
2019-04-08 23:02:46 +0200

    mes problemes!

--- contenidors/simrec1.cc
+++ contenidors/simrec1.cc
@@ -2,26 +2,19 @@
 #include <stack>
 using namespace std;
 
-void sttost(stack<int>& result, stack<int>& source){
-	while(not source.empty()){
-		result.push(source.top());	source.pop();
-	}
-}
-
 void escriu(int n) {
-	if (n > 0) {
-		stack<int> p1;
-		for(int i=1; i<n; ++i){
-			stack<int> p2;
-			p2.push(i);
-			sttost(p2, p1);
-			stack<int> p3 = p2;
-			sttost(p1, p2);
-			sttost(p1, p3);
-		}
-		p1.push(n);
-		while(not p1.empty()){
-			cout << ' ' << p1.top();	p1.pop();
+	stack<int> st;
+	st.push(n);
+	while (not st.empty()) {
+		if (st.top() == 1) {
+			cout << " " << 1;
+			st.pop();
+		} else {
+			int k = st.top();
+			st.pop();
+			cout << " " << k;
+			st.push(k-1);
+			st.push(k-1);
 		}
 	}
 }
